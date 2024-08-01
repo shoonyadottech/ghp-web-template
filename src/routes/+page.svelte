0a09a07fc92e762bbf714e65ee74bb49d8c2bee3
Pushkaraj Shinde <48402034+pshkrjs@users.noreply.github.com>
2024-08-01 10:57:00 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main class="container mx-auto">
+    <div class="grid-cols-5 gap-2">
+        <div class="colspan-5 text-center pt-5 p-2">
+            <h1 class="text-5xl font-extrabold font-sans">Website Template</h1>
+            <p class="font-serif">Ready to use barebones Template Using SvelteKit and Tailwind</p>
+        </div>
+    </div>
+</main>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
+
+<svelte:head>
+    <title>Website Template</title>
+</svelte:head>
